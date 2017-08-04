9616fc58b521c45401f759da5108b18ccb33349d
William Caballero <wcaballero4@gmail.com>
2017-08-03 23:49:32 -0500

    all features working, but small bug: of setState before component is mounted

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import RecentPoints from './recent_points';
+import AllTimePoints from './all_time_points';
+import downArrow from './downArrow.svg';
+import upArrow from './upArrow.svg';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPoints: 'RECENTPOINTS',
+      recentArrow: downArrow,
+      allTimeArrow: upArrow
+    };
+  }
+
+  // method to toggle arrows (down arrow is displayed, up arrow is not displayed)
+  togglePoints(points) {
+    // no need to change the state
+    if(points === this.state.currentPoints){
+      return;
+    }
+    console.log(this.state.currentPoints);
+    console.log(points);
+    // toggles when the arrow position (up/down)
+    if(points === 'RECENTPOINTS'){
+      this.setState({
+        currentPoints: 'RECENTPOINTS',
+        recentArrow: downArrow,
+        allTimeArrow: upArrow
+      });
+    } else {
+      this.setState({
+        currentPoints: 'ALLTIMEPOINTS',
+        recentArrow: upArrow,
+        allTimeArrow: downArrow
+      });
+    }
+  }
+  // renders the component based on the arrow position
+  renderComponent() {
+    // return <AllTimePoints />;
+     if (this.state.currentPoints === 'RECENTPOINTS'){
+       return <RecentPoints />;
+     } else {
+       return <AllTimePoints />;
+     }
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App container">
+        <h2 className="bg-success">Leaderboard</h2>
+        <table className="table table-striped table-sm">
+          <thead>
+            <tr className="table-bordered">
+              <th>#</th>
+              <th>Camper name</th>
+              <th>Points in past 30 days <img src={this.state.recentArrow} alt="RECENTPOINTS" onClick={() => {this.togglePoints("RECENTPOINTS");}}/></th>
+              <th>All time points <img src={this.state.allTimeArrow} alt="ALLTIMEPOINTS" onClick={() => {this.togglePoints("ALLTIMEPOINTS");}}/></th>
+            </tr>
+          </thead>
+          {this.renderComponent()}
+        </table>
       </div>
+
     );
   }
 }
---
+++ src/components/all_time_points.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+
+
+const ROOT_URL = `https://fcctop100.herokuapp.com/api/fccusers/top/alltime`;
+
+export default class RecentPosts extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: []
+    };
+
+    this.renderList = this.renderList.bind(this);
+  }
+
+  //fetch datas from network request
+  fetchUsers() {
+
+    // request is a promise and .then inteprets the promise and stores in a var called data
+    axios.get(ROOT_URL)
+      .then(({data}) => {
+        this.setState({users: data});
+      });
+      return this.renderList();
+  }
+
+  // // outputs data to table (view)
+  renderList() {
+    return this.state.users.map((user, index) => {
+      return (
+        <tr key={user.username}>
+          <td className="text-xs-left">{index + 1}</td>
+          <td className="text-xs-left"><img className="img-responsive img-thumbnail" src={user.img} alt={`img of ${user.username} user`} />{user.username}</td>
+          <td>{user.recent}</td>
+          <td>{user.alltime}</td>
+        </tr>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <tbody id="recentPoints" className="table-bordered">
+        {this.fetchUsers()}
+      </tbody>
+    );
+
+  }
+}
